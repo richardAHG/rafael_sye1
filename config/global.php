6917ad31dee709541066261273323953fa7d9ea7
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-03-10 17:31:36 -0500

    archivo de config default

--- config/global.php
+++ config/global.php
@@ -1,37 +1,16 @@
 <?php 
-// //Ip de la pc servidor de base de datos
-// define("DB_HOST","localhost");
-
-// //Nombre de la base de datos
-// // define("DB_NAME", "dbats");
-// define("DB_NAME", "appssoma_dbats");
-// //Usuario de la base de datos
-// // define("DB_USERNAME", "root");
-// define("DB_USERNAME", "appssoma_wp775");
-// //Contrase�0�9a del usuario de la base de datos
-// // define("DB_PASSWORD", "");
-// define("DB_PASSWORD", "Appsoma%123%2020");
-// //definimos la codificaci��n de los caracteres
-// define("DB_ENCODE","utf8");
-
-// //Definimos una constante como nombre del proyecto
-// define("PRO_NOMBRE","SE");
 
 //Ip de la pc servidor de base de datos
-define("DB_HOST","165.232.137.209:4417");
+define("DB_HOST","");
 
 //Nombre de la base de datos
-// define("DB_NAME", "dbats");
-define("DB_NAME", "db_sye_ssoma");
+define("DB_NAME", "");
 //Usuario de la base de datos
-// define("DB_USERNAME", "root");
-define("DB_USERNAME", "user01");
+define("DB_USERNAME", "");
 //Contrase�0�9a del usuario de la base de datos
-// define("DB_PASSWORD", "");
-define("DB_PASSWORD", "w(6^hc/{Dcn5Be%?");
+define("DB_PASSWORD", "");
 //definimos la codificaci��n de los caracteres
 define("DB_ENCODE","utf8");
-
 //Definimos una constante como nombre del proyecto
 define("PRO_NOMBRE","SE");
 
