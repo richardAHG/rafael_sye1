5168e73874c8fa00caee8f9157546ecd7a659236
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-06-25 17:38:42 -0500

    se retiro restricion de caracteres al crear subactividad

--- vistas/subActividad.php
+++ vistas/subActividad.php
@@ -39,7 +39,7 @@ if ($_SESSION['Administrar']==1)
                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <label>Nombre:</label>
                             <input type="hidden" name="id" id="id">
-                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
+                            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" required>
                           </div>
                           
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
